6e977e4a4065ca6097bfd9cfb474db4e9c148140
doubleam <admin@biugle.cn>
2023-09-18 17:47:25 +0800

    Feat: 登录功能 mock

--- src/services/user.ts
+++ src/services/user.ts
@@ -3,21 +3,22 @@
  * @Author: HxB
  * @Date: 2022-05-07 11:52:00
  * @LastEditors: DoubleAm
- * @LastEditTime: 2022-08-23 18:27:18
+ * @LastEditTime: 2023-09-18 17:25:46
  * @Description: user service
  * @FilePath: \vue-admin\src\services\user.ts
  */
 import XHttp from '@/utils/xhttp';
 
 export function login(data: any) {
-  return import.meta.env.MODE === 'development'
-    ? new Promise((resolve) => {
-        resolve({
-          code: 0,
-          data: {
-            token: 'token',
-          },
-        });
-      })
-    : XHttp.post('/login', data);
+  // return import.meta.env.MODE === 'development'
+  //   ?
+  return new Promise((resolve) => {
+    resolve({
+      code: 0,
+      data: {
+        token: 'token',
+      },
+    });
+  });
+  // : XHttp.post('/login', data);
 }
